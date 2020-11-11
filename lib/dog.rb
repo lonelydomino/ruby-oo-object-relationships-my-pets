407c0ef5376de7f9d1c012ec4662e2bb1d134dd4
Michael Martinez <zeroeclipse@live.com>
2020-11-10 18:48:40 -0800

    commit tree f02dc2a4a0fc72bde752e5cd54cc12b761ae0aa5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = []
+  def initialize(name, owner)
+    @mood = "nervous"
+    @name = name
+    @owner = owner
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
